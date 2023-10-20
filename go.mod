ff781a7ee2d88ffe3551d8fd9d17922143aba612
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-10-19 23:04:41 -0300

    update module: model

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/cdvelop/inputimage
 
 go 1.20
 
-require github.com/cdvelop/model v0.0.57
-
 replace github.com/cdvelop/model => ../model
--- html.go
+++
@@ -1,6 +0,0 @@
-package inputimage
-
-func (i input) HtmlTag(id, field_name string, allow_skip_completed bool) string {
-
-	return `<input id="` + id + `" type="file" id="avatar" name="` + field_name + `" accept="image/,.png,.jpeg,.jpg" />`
-}
